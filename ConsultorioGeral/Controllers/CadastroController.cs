fcbf1c6b599d06220a177c69d20e0ea6729fe89c
Julia Magalhães <juliamagalhaes28@outlook.com>
2020-11-24 21:30:59 -0300

    Entity adicionado

--- ConsultorioGeral/Controllers/CadastroController.cs
+++ ConsultorioGeral/Controllers/CadastroController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConsultorioGeral.Models;
 
 namespace ConsultorioGeral.Controllers
 {
     public class CadastroController : Controller
     {
+        private readonly PacienteDao pacienteDao = new PacienteDao();
         public IActionResult Paciente()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Paciente([Bind("Nome, Cpf, Idade, Email, Sexo, Endereco, Cep, Telefone")] Paciente paciente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await pacienteDao.GravarPaciente(paciente);
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Não foi possível inserir dados. " + e.Message);
+            }
+            return View(paciente);
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(pacienteDao.ObterTodos());
+        }
+
         public IActionResult Consulta()
         {
             return View();
---
+++ ConsultorioGeral/Models/PacienteDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsultorioGeral.Models
+{
+    public class PacienteDao
+    {
+        public PacienteDao()
+        {
+        }
+        private static IList<Paciente> pacientes = new List<Paciente>()
+        {
+            new Paciente()
+            {
+                Nome = "Júlia",
+                Cpf = "02120400000",
+                Idade = "19",
+                Email = "juliatorres203@gmail.com",
+                Sexo = "Feminino",
+                Endereco = "Avenida dos Acadêmicos",
+                Cep = "27175-000",
+                Telefone = "(24)992940332"
+            }
+        };
+
+        public async Task<Paciente> GravarPaciente(Paciente paciente)
+        {
+            pacientes.Add(paciente);
+            return paciente;
+        }
+        public IList<Paciente> ObterTodos()
+        {
+            return pacientes;
+        }
+    }
+}
+
