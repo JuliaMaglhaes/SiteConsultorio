1e8b5076d73edaafb3babd55a994af10fc093e2c
Julia Magalhães <juliamagalhaes28@outlook.com>
2020-12-07 21:04:23 -0300

    correção banco de dados

---
+++ ConsultorioGeral/Migrations/20201207235930_correcao_model.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsultorioGeral.Migrations
+{
+    public partial class correcao_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
